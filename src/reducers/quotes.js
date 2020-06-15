c9bb852020886ba7b2443dfdfe5fa0c0036c7850
edhernandez04 <mchh.004@gmail.com>
2020-06-15 17:01:42 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,20 @@
 export default (state = [], action) => {
-  return state;
+  let quote
+
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote]
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id != action.quoteId)
+    case "UPVOTE_QUOTE":
+      let quoteToVote = state.find(quote => quote.id === action.quoteId)
+        quoteToVote.votes += 1
+          return state
+    case "DOWNVOTE_QUOTE":
+      let quoteToUnVote = state.find(quote => quote.id === action.quoteId)
+        if (quoteToUnVote.votes > 0) {quoteToUnVote.votes -= 1}
+          return state
+    default:
+      return state
+  }
 }
